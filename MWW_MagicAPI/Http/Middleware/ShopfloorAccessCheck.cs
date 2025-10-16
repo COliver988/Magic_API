2f8a281fecd22ba834e4d99c2764e80169aea070
Craig Oliver <coliver988@gmail.com>
2025-10-16 14:55:59 -0400

    set shopfloor test dir to Test as that is what it is Linux

--- MWW_MagicAPI/Http/Middleware/ShopfloorAccessCheck.cs
+++ MWW_MagicAPI/Http/Middleware/ShopfloorAccessCheck.cs
@@ -14,7 +14,7 @@ public class ShopfloorAccessCheck : IHealthCheck
     {
         try
         {
-            string testFile = System.IO.Path.Combine(_path, "test", "healthcheck.txt");
+            string testFile = System.IO.Path.Combine(_path, "Test", "healthcheck.txt");
             File.WriteAllText(testFile, DateTime.Now.ToString());
             if (File.Exists(testFile))
             {
